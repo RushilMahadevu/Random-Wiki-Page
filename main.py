4fdc6ba04cb11df635e856775ae59a22ce5a89b6
Rushil Mahadevu <103291736+Swankk@users.noreply.github.com>
2022-05-29 20:56:36 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,100 @@
+import webbrowser
+
+user_input = input('Do you want to go to an random article (y/n):')
+
+
+if user_input == 'y':
+    webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+    user_input = input('\nDo you want to go to another random article (y/n):')
+    if user_input == 'y':
+        webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+        user_input = input('\nDo you want to go to another random article (y/n):')
+        if user_input == 'y':
+            webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+            user_input = input('\nDo you want to go to another random article (y/n):')
+            if user_input == 'y':
+                webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                user_input = input('\nDo you want to go to another random article (y/n):')
+                if user_input == 'y':
+                    webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                    user_input = input('\nDo you want to go to another random article (y/n):')
+                    if user_input == 'y':
+                        webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                        user_input = input('\nDo you want to go to another random article (y/n):')
+                        if user_input == 'y':
+                            webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                            user_input = input('\nDo you want to go to another random article (y/n):')
+                            if user_input == 'y':
+                                webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                                user_input = input('\nDo you want to go to another random article (y/n):')
+                                if user_input == 'y':
+                                    webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                                    user_input = input('\nDo you want to go to another random article (y/n):')
+                                    if user_input == 'y':
+                                        webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                                        user_input = input('\nDo you want to go to another random article (y/n):')
+                                        if user_input == 'y':
+                                            webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                                            user_input = input('\nDo you want to go to another random article (y/n):')
+                                            if user_input == 'y':
+                                                webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                                                user_input = input('\nDo you want to go to another random article (y/n):')
+                                                if user_input == 'y':
+                                                    webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                                                    user_input = input(
+                                                        '\nDo you want to go to another random article (y/n):')
+                                                    if user_input == 'y':
+                                                        webbrowser.open('https://en.wikipedia.org/wiki/Special:Random')
+                                                        user_input = input(
+                                                            '\nDo you want to go to another random article (y/n):')
+                                                        if user_input == 'y':
+                                                            webbrowser.open(
+                                                                'https://en.wikipedia.org/wiki/Special:Random')
+                                                            user_input = input(
+                                                                '\nDo you want to go to another random article (y/n):')
+                                                            if user_input == 'y':
+                                                                webbrowser.open(
+                                                                    'https://en.wikipedia.org/wiki/Special:Random')
+                                                                user_input = input(
+                                                                    '\nDo you want to go to another random article (y/n):')
+                                                                if user_input == 'y':
+                                                                    webbrowser.open(
+                                                                        'https://en.wikipedia.org/wiki/Special:Random')
+                                                                    user_input = input(
+                                                                        '\nDo you want to go to another random article (y/n):')
+                                                                    if user_input == 'y':
+                                                                        webbrowser.open(
+                                                                            'https://en.wikipedia.org/wiki/Special:Random')
+                                                                        user_input = input(
+                                                                            '\nDo you want to go to another random article (y/n):')
+                                                                        if user_input == 'y':
+                                                                            webbrowser.open(
+                                                                                'https://en.wikipedia.org/wiki/Special:Random')
+                                                                            user_input = input(
+                                                                                '\nDo you want to go to another random article (y/n):')
+                                                                            if user_input == 'y':
+                                                                                webbrowser.open(
+                                                                                    'https://en.wikipedia.org/wiki/Special:Random')
+                                                                                user_input = input(
+                                                                                    '\nDo you want to go to another random article (y/n):')
+                                                                                if user_input == 'y':
+                                                                                    webbrowser.open(
+                                                                                        'https://en.wikipedia.org/wiki/Special:Random')
+                                                                                    user_input = input(
+                                                                                        '\nDo you want to go to another random article (y/n):')
+                                                                                    if user_input == 'y':
+                                                                                        webbrowser.open(
+                                                                                            'https://en.wikipedia.org/wiki/Special:Random')
+                                                                                        user_input = input(
+                                                                                            '\nDo you want to go to another random article (y/n):')
+                                                                                        if user_input == 'y':
+                                                                                            webbrowser.open(
+                                                                                                'https://en.wikipedia.org/wiki/Special:Random')
+                                                                                            user_input = input(
+                                                                                                '\nDo you want to go to another random article (y/n):')
+                                                                                            if user_input == 'y':
+                                                                                                webbrowser.open(
+                                                                                                    'https://en.wikipedia.org/wiki/Special:Random')
+
+if user_input == 'no':
+    pass
